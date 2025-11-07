e523b2576620b3b93bfc7790fca84f5f352a5e8e
manxiaoboo <manxiaobooo@126.com>
2025-11-07 14:53:27 +0800

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,6 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  imports: [CommonModule]
 })
 export class App {
+  private http = inject(HttpClient);
+
   protected readonly title = signal('cloudflare-host-app');
+
+  public orders = this.http.get('https://bbf.anna091438.workers.dev/proxy-http/orders').pipe(map((data: any) => data.payload));
+
 }
